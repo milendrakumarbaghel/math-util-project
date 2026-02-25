382d0201fee9de13f36e4d134b435c422b1e6703
HarshitsSri <srivastavh757@gmail.com>
2026-02-25 16:57:23 +0530

    Division code done

---
+++ Division.java
@@ -0,0 +1,10 @@
+public class Division {
+
+    
+    public static double divide(double a, double b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return a / b;
+    }
+}
